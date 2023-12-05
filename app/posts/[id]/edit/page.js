0095ab269e3f7aedf4bbcad2b11b816c86f1e33f
dmrado <dmrado@live.ru>
2023-12-05 21:55:37 +0300

    адаптивная верстка главной страницы footer, mission

--- app/posts/[id]/edit/page.js
+++ app/posts/[id]/edit/page.js
@@ -11,12 +11,13 @@ const getPostById = async (id) => {
 
 const updatePost = async (data) => {
     'use server'
-    const {title, body, id} = Object.fromEntries(data)
+    // e.preventDefault()
+    const {title, text, id} = Object.fromEntries(data)
     console.log('id', id)
     const response = await fetch(BACKEND_URL + `/posts/${id}`, {
         method: 'PUT',
         headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify(title, body)
+        body: JSON.stringify(title, text)
     })
     const updatedPost = response.json()
     revalidatePath(`/posts/${updatedPost.id}`)
@@ -26,12 +27,13 @@ const updatePost = async (data) => {
 const Page = async ({params: {id}}) => {
     const post = await getPostById(id)
     console.log(post)
-    //todo рендерится не то же что в phpMyAdmin
+    //todo рендерится не то же что в phpMyAdmin только id правильный
     return (<>
             <h1>Редактирование поста {post.title}</h1>
+            <h2>post id: {post.id}</h2>
             <form className="form" action={updatePost}>
                 <input defaultValue={post.title}  type="text" placeholder="Заголовок" required name="title"/>
-                <textarea defaultValue={post.body} placeholder="Ваш пост" required name="body"/>
+                <textarea defaultValue={post.text} placeholder="Ваш пост" rows="10" cols="80" name="text"/>
                 <input type="hidden" name={id} value={post.id}/>
                 <div>
                     <input type="submit" value="Отправить"/>
--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,86 @@
+import Link from "next/link";
+
 const Footer = () => {
     const date = new Date()
     const fullYear = date.getFullYear()
 
     return (<>
-            <footer className="footer">Designed by Polina&copy; Powered by Dm&copy; {fullYear}</footer>
+            <footer className="footer">
+
+                <Link href="/" className="logo">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={69}
+                        height={71}
+                        viewBox="0 0 69 71"
+                        fill="none"
+                    >
+                        <path
+                            d="M34.9408 5L63.5709 50.6217H6.31069L34.9408 5Z"
+                            stroke="#004E98"
+                            strokeWidth={5}
+                        />
+                        <path
+                            d="M34.0592 65.829L5.42908 20.2073L62.6893 20.2073L34.0592 65.829Z"
+                            stroke="#004E98"
+                            strokeWidth={5}
+                        />
+                    </svg>
+
+                </Link>
+
+                <div className="nav-text">
+                    <p className="nav-text_1">Бейт-Иешуа</p>
+                    <p className="nav-text_2">Синагога г. Артем</p>
+                </div>
+
+
+                <div className="footer__info">
+                    <div className="footer__address">
+                        <span>Адрес</span><br/>
+                        г. Артем, Приморский край<br/>
+                        <span>Телефон</span> <br/>
+                        +7(9987) 777-87-87
+                    </div>
+
+
+                    <div className="footer__socials">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={36}
+                            height={36}
+                            viewBox="0 0 36 36"
+                            fill="none"
+                        >
+                            <circle cx={18} cy={18} r={18} fill="#004E98"/>
+                        </svg>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={36}
+                            height={36}
+                            viewBox="0 0 36 36"
+                            fill="none"
+                        >
+                            <circle cx={18} cy={18} r={18} fill="#004E98"/>
+                        </svg>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={36}
+                            height={36}
+                            viewBox="0 0 36 36"
+                            fill="none"
+                        >
+                            <circle cx={18} cy={18} r={18} fill="#004E98"/>
+                        </svg>
+                    </div>
+                </div>
+
+                <div className="footer_signature">
+                    Designed by Polina&copy; Powered by Dm&copy; {fullYear}
+                </div>
+            </footer>
         </>
     );
 };
