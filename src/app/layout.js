3a8544145a8717bbc69149b5413646c64b62cb8c
dmrado <dmrado@live.ru>
2024-02-01 10:29:28 +0300

    yandex metrika

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,24 @@ export default function RootLayout({children}) {
         <html lang="en">
 
         <body className={inter.className}>
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" defer>
+            {(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")}
+
+            {ym(96326668, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        })}
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/96326668" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+
         <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
                 async defer>
         </script>
